e36ddb6a6bce7988d8573d36a45fc98f8be74006
clare <cbwood04@icloud.com>
2025-11-27 22:27:14 -0700

    header and footer added to app.jsx.

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,28 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
+  return (
+    <div>
+      <header>
+
+        <h1>Startup</h1>
+
+      <nav>
+        <menu>
+          <li><a href="index.html">Home</a></li>
+          <li><a href="today.html">Today</a></li>
+          <li><a href="history.html">History</a></li>
+          <li><a href="popular.html">Popular</a></li>
+        </menu>
+      </nav>
+
+    </header>
+
+      <main>App components go here</main>
+
+      <footer>
+      <a href="https://github.com/clarebwood/startup.git">Clare Wood (Source)</a>
+    </footer>
+    </div>
+  );
 }
