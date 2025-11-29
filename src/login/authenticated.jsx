c59208d0e903a59d892f213ab17fd5bce555e47d
clare <cbwood04@icloud.com>
2025-11-28 20:10:13 -0700

    copied files from simon.

---
+++ src/login/authenticated.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import Button from 'react-bootstrap/Button';
+
+import './authenticated.css';
+
+export function Authenticated(props) {
+  const navigate = useNavigate();
+
+  function logout() {
+    localStorage.removeItem('userName');
+    props.onLogout();
+  }
+
+  return (
+    <div>
+      <form>
+        <div>
+          <input type="text" placeholder="username" />
+        </div>
+        <div>
+          <input type="password" placeholder="password" />
+        </div>
+        <button type="submit">Login</button>
+        <button type="submit">Create</button>
+      </form>
+    </div>
+  );
+}
